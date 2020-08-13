a65b5f81b7c5854b59181b3c4ea9935223fd1ccc
Lokixa <lyuben.kodinov@gmail.com>
2020-08-13 16:48:32 +0300

    Added config file

--- src/Services/Db.tsx
+++ src/Services/Db.tsx
@@ -1,7 +1,8 @@
 import { Project } from '../Content/Pages/Projects/Project'
+import Config from '../Services/config.json'
 
 class DbServices {
-	private static url: string = 'https://api.lyubenk.com/'
+	private static url: string = Config.BackendURL
 	static getProjects(): Promise<Project[]> {
 		return fetch(this.url + 'projects').then(
 			(data) => data.json() as Promise<Project[]>
