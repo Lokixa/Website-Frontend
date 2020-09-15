4fc397b9f3255f05f9de3ac08153a0bc50de8035
Lykod <lyuben.kodinov@gmail.com>
2020-09-15 19:32:34 +0300

    removed builds

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/cool-things":["static\u002Fchunks\u002Fpages\u002Fcool-things.js"],"/projects":["static\u002Fchunks\u002Fpages\u002Fprojects.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fcool-things","\u002Fprojects"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
